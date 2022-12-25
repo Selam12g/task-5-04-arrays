a1c036b08c4dfda99ee1a41d2bf79bdc3b53e8d5
Dell <Dell@DESKTOP-9319AP6>
2022-12-25 14:42:50 +0100

    Add project files.

---
+++ task 5   04 arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using System;
+public class program
+{
+    public static void Main(string[] args)
+    {
+        int a, i, p, k;
+        Console.WriteLine("Inter a number;");
+        a = int.Parse(Console.ReadLine());  
+        for (i = 2; i <= a; i++)
+        {
+            k = 2;
+            p = 1;
+            while (k < i)
+            {
+                if (i % k ==0)
+                {
+                    p = 0; 
+                    break;
+                }
+                k++;
+            }
+            if (p == 1)
+            {
+                Console.WriteLine(i + "= prime");
+                
+            }
+        } 
+
+    }
+}
